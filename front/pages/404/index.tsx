8875c7a838c60b06529643d176655f00d1e3769f
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-07-21 00:16:17 +0200

    add 404 page

---
+++ front/pages/404/index.tsx
@@ -0,0 +1,29 @@
+import { PageLayout } from '@components'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+import s from './styles.module.scss'
+
+const FourOhFour: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>🙃 Oops</title>
+      </Head>
+      <PageLayout>
+        <h1 className={s.error}>
+          🙃 Sorry, I was too lazy to make the page you're looking for <br />
+          Feel free to remind me to work more{' '}
+          <a
+            href="https://github.com/quentingrchr/simply/issues"
+            target="_blank"
+          >
+            here
+          </a>
+        </h1>
+      </PageLayout>
+    </>
+  )
+}
+
+export default FourOhFour
