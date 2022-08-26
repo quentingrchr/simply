291609f8867a4d8a5ae5d7810ce130c47af61fe7
Quentin Grnchr <quentin.grancher@ultranoir.com>
2022-08-26 19:18:26 +0200

    file upload is not handle by an external provider

--- back/config/middlewares.js
+++ back/config/middlewares.js
@@ -1,6 +1,16 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
