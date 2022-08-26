a2228df13f9303d514d994bc7fe010b6eaba8e00
Quentin Grnchr <quentin.grancher@ultranoir.com>
2022-08-26 14:51:33 +0200

    update back models

---
+++ back/src/api/jewelry-type/controllers/jewelry-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  jewelry-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::jewelry-type.jewelry-type');
---
+++ back/src/api/jewelry-type/services/jewelry-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * jewelry-type service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::jewelry-type.jewelry-type');
