d89ab96312263e1154ec5c603aa604bfc055f89f
Quentin Grnchr <quentin.grancher@ultranoir.com>
2022-09-24 15:17:01 +0200

    fix safari mobile

--- front/components/modal/index.tsx
+++ front/components/modal/index.tsx
@@ -42,6 +42,8 @@ export default function Modal({
   useEffect(() => {
     if (activeModal === id) {
       setScrollDisabled(true)
+    } else {
+      setScrollDisabled(false)
     }
   }, [activeModal])
 
